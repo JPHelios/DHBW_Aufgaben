124fe1708e019dc12ae3924d6a5e5aaa6950cc53
JPHelios <43265991+JPHelios@users.noreply.github.com>
2021-02-09 21:58:46 +0100

    String und Methods HA beenden

--- src/de/dhbw/programmieren/java/exercise/methods/Quicksort.java
+++ src/de/dhbw/programmieren/java/exercise/methods/Quicksort.java
@@ -1,17 +1,57 @@
 package de.dhbw.programmieren.java.exercise.methods;
 
+import java.util.Arrays;
+
 public class Quicksort {
 
 
     public static void main(String[] args) {
-        int[] array = {9, 2, 5, 7, 8, 3, 4, 10, 1, 6};
+        int[] daten = {9, 2, 5, 7, 8, 3, 4, 10, 1, 6};
+
+        System.out.println(Arrays.toString(daten));
+        quicksort(0, daten.length-1, daten);
+        System.out.println(Arrays.toString(daten));
 
 
     }
 
-    public static void sort(int[] array){
+    public static void quicksort(int links, int rechts, int[] daten){
 
+        if(links < rechts){
 
-        
+            int teiler = teile(links,rechts, daten);
+            quicksort(links, teiler-1, daten);
+            quicksort(teiler+1, rechts, daten);
+
+        }
+    }
+
+    public static int teile(int links, int rechts, int[] daten){
+
+        int i = links;
+        int j = rechts - 1;
+        int pivot = daten[rechts];
+
+        do {
+            while (daten[i] <= pivot & i < rechts) i = i + 1;
+            while (daten[j] >= pivot & j > links) j = j - 1;
+
+            if (i < j) {
+                int tmp = daten[i];
+                daten[i] = daten[j];
+                daten[j] = tmp;
+
+            }
+        } while (i < j);
+
+        if(daten[i] > pivot){
+
+            int tmp = daten[i];
+            daten[i] = daten[rechts];
+            daten[rechts] = tmp;
+
+        }
+
+        return i;
     }
 }
--- src/de/dhbw/programmieren/java/exercise/strings/RomanNumber.java
+++ src/de/dhbw/programmieren/java/exercise/strings/RomanNumber.java
@@ -4,12 +4,73 @@ import java.util.Scanner;
 
 public class RomanNumber {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Bitte geben Sie eine röm. Zahl ein:");
-        String input = scanner.next();
+        StringBuffer input = new StringBuffer();
+        StringBuffer cutOut = new StringBuffer();
 
+        String staticInput = input.toString();
+
+        input.append(scanner.next());
+
+        int wert = 0;
+
+        for(int i = 1; i < input.length(); i++){
+
+            String tmp = input.charAt(i - 1) + String.valueOf(input.charAt(i));
+
+            switch (tmp) {
+                case "IV" -> {
+                    wert = wert + 4;
+                    cutOut.append(i - 1).append(i);
+                }
+                case "IX" -> {
+                    wert = wert + 9;
+                    cutOut.append(i - 1).append(i);
+                }
+                case "XL" -> {
+                    wert = wert + 40;
+                    cutOut.append(i - 1).append(i);
+                }
+                case "XC" -> {
+                    wert = wert + 90;
+                    cutOut.append(i - 1).append(i);
+                }
+                case "CD" -> {
+                    wert = wert + 400;
+                    cutOut.append(i - 1).append(i);
+                }
+                case "CM" -> {
+                    wert = wert + 900;
+                    cutOut.append(i - 1).append(i);
+                }
+            }
+        }
+
+        String cutOutString = cutOut.toString();
+
+        for(int i = cutOut.length()-1; i >= 0; i--){
+            int tmpInt = Integer.valueOf(String.valueOf(cutOutString.charAt(i)));
+            input.deleteCharAt(tmpInt);
+        }
+
+        for(int i = 0; i < input.length(); i++){
+            String tmp = String.valueOf(input.charAt(i));
+
+            switch (tmp){
+                case "M" -> wert = wert + 1000;
+                case "D" -> wert = wert + 500;
+                case "C" -> wert = wert + 100;
+                case "L" -> wert = wert + 50;
+                case "X" -> wert = wert + 10;
+                case "V" -> wert = wert + 5;
+                case "I" -> wert = wert + 1;
+            }
+        }
+
+        System.out.println("Der Wert der Zahl " + staticInput + " ist " + wert);
     }
-
 }
